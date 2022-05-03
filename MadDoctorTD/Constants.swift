2b8179225d8a8105f1665a1e6b3da5a2eea614c5
Luca Salmi <luca.salmi90@gmail.com>
2022-05-03 14:32:02 +0200

    constants file

---
+++ MadDoctorTD/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  MadDoctorTD
+//
+//  Created by Luca Salmi on 2022-05-03.
+//
+
+import Foundation
+
+
+enum AppState: Int{
+    
+    case startMenu = 0 , labMenu, gameScene, researchMenu, settingsMenu
+    
+}
+
+enum GameState: Int{
+    
+    case menus = 0 , build, wave, pause
+    
+}
+
+//Towers
+
+
+//Bullets
+
+
+//Enemies
+
+
