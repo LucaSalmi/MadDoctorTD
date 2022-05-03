e0fb3535b4af7a77c2f35aab0a60e2af53baea41
Luca Salmi <luca.salmi90@gmail.com>
2022-05-03 12:02:18 +0200

    Initial Commit

---
+++ MadDoctorTD/MadDoctorTDApp.swift
@@ -0,0 +1,20 @@
+//
+//  MadDoctorTDApp.swift
+//  MadDoctorTD
+//
+//  Created by Luca Salmi on 2022-05-03.
+//
+
+import SwiftUI
+
+@main
+struct MadDoctorTDApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
