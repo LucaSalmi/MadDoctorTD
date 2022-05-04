98318335c27e2c95d33a975b920ef6d332e34212
Daniel Falkedal <daniel.falkedal@gmail.com>
2022-05-04 16:37:13 +0200

    added subclass gun projectile

---
+++ MadDoctorTD/GunProjectile.swift
@@ -0,0 +1,26 @@
+//
+//  GunProjectile.swift
+//  MadDoctorTD
+//
+//  Created by Daniel Falkedal on 2022-05-04.
+//
+
+import Foundation
+import SpriteKit
+
+class GunProjectile: Projectile {
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("use init()")
+    }
+    
+    override init(position: CGPoint, target: Enemy){
+        
+        super.init(position: position, target: target)
+        
+        texture = SKTexture(imageNamed: "test_projectile")
+
+        
+    }
+    
+}
