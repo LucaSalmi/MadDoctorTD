60621a19d5bf419ced78136cc64ec051e7397a87
Luca Salmi <luca.salmi90@gmail.com>
2022-05-04 13:15:02 +0200

    enemy class

---
+++ MadDoctorTD/Enemy.swift
@@ -0,0 +1,38 @@
+//
+//  Enemy.swift
+//  MadDoctorTD
+//
+//  Created by Luca Salmi on 2022-05-04.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+
+class Enemy: SKSpriteNode{
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("use init()")
+    }
+    
+    init(texture: SKTexture, color: UIColor){
+        
+        let tempColor = UIColor(.indigo)
+        super.init(texture: texture, color: tempColor, size: EnemiesData.size)
+        physicsBody = SKPhysicsBody(circleOfRadius: size.width/2)
+        physicsBody?.categoryBitMask = PhysicsCategory.Enemy
+        physicsBody?.collisionBitMask = PhysicsCategory.Foundation
+        physicsBody?.restitution = 0
+        physicsBody?.allowsRotation = false
+        
+    }
+    
+    func update(graph : GKObstacleGraph<GKGraphNode2D>){
+        
+        self.position = CGPoint(x: self.position.x, y: self.position.y + EnemiesData.baseSpeed)
+        
+        
+    }
+    
+}
+
