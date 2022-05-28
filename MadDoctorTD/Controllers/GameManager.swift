a4b19359e06a14259be50939c346f40b56a8e3d2
Daniel Falkedal <daniel.falkedal@gmail.com>
2022-05-28 10:36:16 +0200

    removed old code

--- MadDoctorTD/Controllers/GameManager.swift
+++ MadDoctorTD/Controllers/GameManager.swift
@@ -33,34 +33,6 @@ class GameManager: ObservableObject{
     @Published var baseHp: Int = PlayerData.BASE_HP
     @Published var baseHPLost: Int = 0
     
-//    //Gun Tower Unlocks
-//    @Published var gunTowerUnlocked: Bool = true
-//    @Published var gunTowerDamageUnlocked: Bool = false
-//    @Published var gunTowerSpeedUnlocked: Bool = false
-//    @Published var gunTowerRangeUnlocked: Bool = false
-//    @Published var bouncingProjectilesUnlocked: Bool = false
-//
-//    //Rapid Fire Tower Unlocks
-//    @Published var rapidFireTowerUnlocked: Bool = false
-//    @Published var rapidFireTowerDamageUnlocked: Bool = false
-//    @Published var rapidFireTowerSpeedUnlocked: Bool = false
-//    @Published var rapidFireTowerRangeUnlocked: Bool = false
-//    @Published var slowProjectilesUnlocked: Bool = false
-//
-//    //Cannon Tower Unlocks
-//    @Published var cannonTowerUnlocked: Bool = false
-//    @Published var cannonTowerDamageUnlocked: Bool = false
-//    @Published var cannonTowerSpeedUnlocked: Bool = false
-//    @Published var cannonTowerRangeUnlocked: Bool = false
-//    @Published var mineProjectilesUnlocked: Bool = false
-//
-//    //Sniper Tower Unlocks
-//    @Published var sniperTowerUnlocked: Bool = false
-//    @Published var sniperTowerDamageUnlocked: Bool = false
-//    @Published var sniperTowerSpeedUnlocked: Bool = false
-//    @Published var sniperTowerRangeUnlocked: Bool = false
-//    @Published var poisonProjectilesUnlocked: Bool = false
-    
     @Published var currentLevel = 2
     
     //SINGLETON
@@ -98,30 +70,6 @@ class GameManager: ObservableObject{
         LabSceneCommunicator.instance.rapidTowerResearchLevel.removeAll()
         LabSceneCommunicator.instance.cannonTowerResearchLevel.removeAll()
         LabSceneCommunicator.instance.sniperTowerResearchLevel.removeAll()
-        
-//        gunTowerUnlocked = true
-//        gunTowerDamageUnlocked = false
-//        gunTowerSpeedUnlocked = false
-//        gunTowerRangeUnlocked = false
-//        bouncingProjectilesUnlocked = false
-//        
-//        rapidFireTowerUnlocked = false
-//        rapidFireTowerDamageUnlocked = false
-//        rapidFireTowerSpeedUnlocked = false
-//        rapidFireTowerRangeUnlocked = false
-//        slowProjectilesUnlocked = false
-//        
-//        cannonTowerUnlocked = false
-//        cannonTowerDamageUnlocked = false
-//        cannonTowerSpeedUnlocked = false
-//        cannonTowerRangeUnlocked = false
-//        mineProjectilesUnlocked = false
-//        
-//        sniperTowerUnlocked = false
-//        sniperTowerDamageUnlocked = false
-//        sniperTowerSpeedUnlocked = false
-//        sniperTowerRangeUnlocked = false
-//        poisonProjectilesUnlocked = false
         
     }
     
