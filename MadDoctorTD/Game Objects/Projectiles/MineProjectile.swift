af763447f38acbe0fa93da155d951549c4c6481e
Daniel Falkedal <daniel.falkedal@gmail.com>
2022-05-30 09:55:41 +0200

    particle nil check

--- MadDoctorTD/Game Objects/Projectiles/MineProjectile.swift
+++ MadDoctorTD/Game Objects/Projectiles/MineProjectile.swift
@@ -99,15 +99,20 @@ class MineProjectile: AoeProjectile{
         let gameScene = GameScene.instance!
         
        // SoundManager.playMortarSwooshSFX()
-        SoundManager.playSFX(sfxName: SoundManager.cannonTowerImpactSFX, scene: GameScene.instance!, sfxExtension: SoundManager.mp3Extension)
-                let particle = SKEmitterNode(fileNamed: "CannonExplosion")
-                particle!.position = position
-                particle!.zPosition = 5
-                gameScene.addChild(particle!)
+        SoundManager.playSFX(
+            sfxName: SoundManager.cannonTowerImpactSFX,
+            scene: GameScene.instance!, sfxExtension: SoundManager.mp3Extension
+        )
+        if let particle = SKEmitterNode(fileNamed: "CannonExplosion") {
+            particle.position = position
+            particle.zPosition = 5
+            gameScene.addChild(particle)
 
-                gameScene.run(SKAction.wait(forDuration: 1)) {
-                    particle!.removeFromParent()
-                }
+            gameScene.run(SKAction.wait(forDuration: 1)) {
+                particle.removeFromParent()
+            }
+        }
+                
         
         self.removeFromParent()
         
